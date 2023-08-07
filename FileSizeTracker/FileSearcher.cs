b3e14190cbd326dfb74651547b16a35769daa518
Ali <alikerimli94@gmail.com>
2023-08-07 11:18:46 +0200

    init

---
+++ FileSizeTracker/FileSearcher.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileSizeTracker;
+
+public class FileSearcher
+{
+    private BackgroundWorker worker;
+    private CancellationTokenSource cts;
+
+    public string DriveOrVolume { get; set; }
+
+    public event ProgressChangedEventHandler ProgressChanged;
+
+    public event EventHandler<ResultEventArgs> ResultFound;
+
+    public event RunWorkerCompletedEventHandler SearchCompleted;
+
+    public FileSearcher(string driveOrVolume)
+    {
+        DriveOrVolume = driveOrVolume;
+
+        worker = new BackgroundWorker();
+        worker.WorkerReportsProgress = true;
+        worker.WorkerSupportsCancellation = true;
+        worker.DoWork += Worker_DoWork;
+        worker.ProgressChanged += Worker_ProgressChanged;
+        worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+
+        cts = new CancellationTokenSource();
+    }
+
+    public void Start()
+    {
+        if (!worker.IsBusy)
+        {
+            worker.RunWorkerAsync();
+        }
+        else if (cts.IsCancellationRequested)
+        {
+            cts = new CancellationTokenSource();
+        }
+    }
+
+    public void Pause()
+    {
+        cts.Cancel();
+    }
+
+    public void Cancel()
+    {
+        cts.Cancel();
+        worker.CancelAsync();
+    }
+
+private void Worker_DoWork(object sender, DoWorkEventArgs e)
+{
+    BackgroundWorker worker = sender as BackgroundWorker;
+    
+    var directories = Directory.GetDirectories(DriveOrVolume, "*", new EnumerationOptions
+    {
+        IgnoreInaccessible = true,
+        RecurseSubdirectories = true
+    });
+    
+    int directoryCount = directories.Count();
+    int processedDirectories = 0;
+    object lockObject = new object();
+
+    Parallel.ForEach(directories, (directory) =>
+    {
+        if (worker.CancellationPending)
+        {
+            e.Cancel = true;
+            return;
+        }
+
+        while (cts.Token.IsCancellationRequested)
+        {
+            Thread.Sleep(100);
+        }
+
+        try
+        {
+            string[] files = Directory.GetFiles(directory);
+
+            bool hasLargeFile = files.Any(file =>
+            {
+                try
+                {
+                    return new FileInfo(file).Length > 10 * 1024 * 1024; // If the file size is larger than 10 MB
+                }
+                catch
+                {
+                    // If an exception occurs, ignore it and continue with the next file 
+                    return false;
+                }
+            });
+
+            if (hasLargeFile)
+            {
+                string[] allFiles = Directory.GetFiles(directory, "*", SearchOption.AllDirectories);
+
+                int fileCount = allFiles.Length;
+
+                long fileSizeSum = allFiles.Sum(file =>
+                {
+                    try
+                    {
+                        return new FileInfo(file).Length;
+                    }
+                    catch
+                    {
+                        // If an exception occurs, ignore it and continue with the next file 
+                        return 0;
+                    }
+                });
+
+                lock (lockObject)
+                {
+                    worker.ReportProgress(processedDirectories * 100 / directoryCount,
+                        new ResultEventArgs(directory, fileCount, fileSizeSum));
+                }
+            }
+        }
+        catch
+        {
+            // If an exception occurs, ignore it and continue with the next directory 
+        }
+
+        lock (lockObject)
+        {
+            processedDirectories++;
+            worker.ReportProgress(processedDirectories * 100 / directoryCount);
+        }
+    });
+}
+    
+    private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+    {
+        ProgressChanged?.Invoke(this, e);
+
+        if (e.UserState != null && e.UserState is ResultEventArgs)
+        {
+            ResultFound?.Invoke(this, e.UserState as ResultEventArgs);
+        }
+    }
+
+    private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+    {
+        SearchCompleted?.Invoke(this, e);
+    }
+}
+
+public class ResultEventArgs : EventArgs
+{
+    public string Path { get; set; }
+
+    public int FileCount { get; set; }
+
+    public long FileSizeSum { get; set; }
+
+    public ResultEventArgs(string path, int fileCount, long fileSizeSum)
+    {
+        Path = path;
+        FileCount = fileCount;
+        FileSizeSum = fileSizeSum;
+    }
+}
---
+++ FileSizeTracker/MainWindow.xaml.cs
@@ -0,0 +1,293 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FileSizeTracker
+{
+    public partial class MainWindow : Window
+    {
+        private FileSearcher fileSearcher;
+        private bool isSearching;
+        private List<ResultItem> resultItems;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            PopulateDriveList();
+            isSearching = false;
+            resultItems = new List<ResultItem>();
+        }
+
+        private void PopulateDriveList()
+        {
+            DriveList.Items.Clear();
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                if (drive.IsReady)
+                {
+                    DriveList.Items.Add(drive);
+                }
+                else
+                {
+                    foreach (string volume in Directory.GetDirectories(drive.Name))
+                    {
+                        DriveList.Items.Add(new VolumeInfo(volume));
+                    }
+                }
+            }
+        }
+
+        private void DriveList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SearchButton.IsEnabled = DriveList.SelectedItem != null;
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (isSearching)
+            {
+                fileSearcher.Pause();
+                SearchButton.Content = "Resume Search";
+                isSearching = false;
+            }
+            else
+            {
+                if (fileSearcher == null)
+                {
+                    fileSearcher = new FileSearcher(DriveList.SelectedItem.ToString());
+                    fileSearcher.ProgressChanged += FileSearcher_ProgressChanged;
+                    fileSearcher.ResultFound += FileSearcher_ResultFound;
+                    fileSearcher.SearchCompleted += FileSearcher_SearchCompleted;
+                }
+                fileSearcher.Start();
+                SearchButton.Content = "Pause Search";
+                isSearching = true;
+            }
+        }
+
+        private void FileSearcher_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            SearchProgress.Value = e.ProgressPercentage;
+        }
+
+        private void FileSearcher_ResultFound(object sender, ResultEventArgs e)
+        {
+            var resultItem = new ResultItem(e.Path, e.FileCount, e.FileSizeSum);
+            resultItems.Add(resultItem);
+            ResultList.Items.Add(resultItem);
+
+            var watcher = new FileSystemWatcher(e.Path);
+            watcher.IncludeSubdirectories = true;
+            watcher.NotifyFilter = NotifyFilters.FileName | NotifyFilters.Size | NotifyFilters.DirectoryName;
+            watcher.Changed += Watcher_Changed;
+            watcher.Created += Watcher_Changed;
+            watcher.Deleted += Watcher_Deleted;
+            watcher.Renamed += Watcher_Renamed;
+
+            watcher.EnableRaisingEvents = true;
+
+            resultItem.Watcher = watcher;
+
+        }
+
+
+        private void FileSearcher_SearchCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                MessageBox.Show("The search was cancelled.", "File Search App", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else if (e.Error != null)
+            {
+                MessageBox.Show("An error occurred during the search.", "File Search App", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                MessageBox.Show("The search completed successfully.", "File Search App", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            SearchButton.Content = "Start Search";
+            isSearching = false;
+        }
+
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            var resultItem = resultItems.FirstOrDefault(item => item.Watcher == sender);
+            resultItem?.Update();
+        }
+
+        private void Watcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            var resultItem = resultItems.FirstOrDefault(item => item.Watcher == sender);
+            
+            if (resultItem == null) return;
+            
+            if (e.FullPath == resultItem.Path)
+            {
+                ResultList.Items.Remove(resultItem);
+                resultItems.Remove(resultItem);
+                resultItem.Watcher.Dispose();
+            }
+            else
+            {
+                resultItem.Update();
+            }
+        }
+
+        private void Watcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            var resultItem = resultItems.FirstOrDefault(item => item.Watcher == sender);
+            
+            if (resultItem == null) return;
+            
+            if (e.OldFullPath == resultItem.Path)
+            {
+                resultItem.Path = e.FullPath;
+            }
+            else
+            {
+                resultItem.Update();
+            }
+        }
+    }
+
+    public class VolumeInfo
+    {
+        public string Name { get; set; }
+
+        public DriveType DriveType { get; set; }
+
+        public string DriveFormat { get; set; }
+
+        public long TotalSize { get; set; }
+
+        public long TotalFreeSpace { get; set; }
+
+        public VolumeInfo(string path)
+        {
+            Name = path;
+
+            var drive = new DriveInfo(Path.GetPathRoot(path));
+
+            DriveType = drive.DriveType;
+
+            try
+            {
+                DriveFormat = drive.DriveFormat;
+            }
+            catch
+            {
+                DriveFormat = "Unknown";
+            }
+
+            try
+            {
+                TotalSize = drive.TotalSize;
+            }
+            catch
+            {
+                TotalSize = 0;
+            }
+
+            try
+            {
+                TotalFreeSpace = drive.TotalFreeSpace;
+            }
+            catch
+            {
+                TotalFreeSpace = 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+
+
+    public class ResultItem : INotifyPropertyChanged
+    {
+        private string path;
+        public string Path
+        {
+            get { return path; }
+            set
+            {
+                if (path == value) return;
+                path = value;
+                OnPropertyChanged(nameof(Path));
+            }
+        }
+
+        private int fileCount;
+        public int FileCount
+        {
+            get => fileCount;
+            set
+            {
+                if (fileCount == value) return;
+                fileCount = value;
+                OnPropertyChanged(nameof(FileCount));
+            }
+        }
+
+        private long fileSizeSum;
+        public long FileSizeSum
+        {
+            get { return fileSizeSum; }
+            set
+            {
+                if (fileSizeSum == value) return;
+                fileSizeSum = value;
+                OnPropertyChanged(nameof(FileSizeSum));
+            }
+        }
+
+        public FileSystemWatcher Watcher { get; set; }
+
+        public ResultItem(string path, int fileCount, long fileSizeSum)
+        {
+            Path = path;
+            FileCount = fileCount;
+            FileSizeSum = fileSizeSum;
+        }
+
+        public void Update()
+        {
+            try
+            {
+                string[] files = Directory.GetFiles(Path, "*", SearchOption.AllDirectories);
+
+                FileCount = files.Length;
+
+                long sizeSum = 0;
+
+                foreach (string file in files)
+                {
+                    try
+                    {
+                        sizeSum += new FileInfo(file).Length;
+                    }
+                    catch
+                    {
+                    }
+                }
+
+                FileSizeSum = sizeSum;
+            }
+            catch
+            {
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
